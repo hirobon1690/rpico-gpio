57b1050b84e9cc409dac24004937a06f3aab229c
hirobon1690 <hirobon1690@gmail.com>
2022-12-13 02:08:00 +0900

    UPDATE: gpio OK

--- gpio.h
+++ gpio.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <stdio.h>
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
 
+class Gpio {
+   public:
+    Gpio(int pin,int mode);
+    void init();
+    void write(bool val);
+    bool read();
+    void toggle();
+
+   private:
+    int pin;
+    int mode;
+    bool state;
+};
+
+const int INPUT = 0;
+const int OUTPUT = 1;
+const int INPUT_PU = 2;
+const int INPUT_PD = 3;
