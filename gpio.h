9c6f18ef9c8c772fb57128fb1674c8bf2cca7c75
hirobon1690 <hirobon1690@gmail.com>
2023-01-03 12:25:16 +0900

    ok

--- gpio.h
+++ gpio.h
@@ -2,10 +2,15 @@
 #include <stdio.h>
 #include "hardware/gpio.h"
 #include "pico/stdlib.h"
+const int INPUT = 0;
+const int OUTPUT = 1;
+const int INPUT_PU = 2;
+const int INPUT_PD = 3;
+
 
 class Gpio {
    public:
-    Gpio(int pin,int mode);
+    Gpio(int pin,int mode=OUTPUT);
     void init();
     void write(bool val);
     bool read();
@@ -17,7 +22,3 @@ class Gpio {
     bool state;
 };
 
-const int INPUT = 0;
-const int OUTPUT = 1;
-const int INPUT_PU = 2;
-const int INPUT_PD = 3;
